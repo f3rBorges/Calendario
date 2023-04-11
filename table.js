21f886d921907c353b9eb62502c7211a83b2c7ba
Fernanda <borges.p@alunoescolassociais.org.br>
2023-04-11 12:40:37 -0300

    arquivos do projeto modirivados

---
+++ table.js
@@ -0,0 +1,10 @@
+function colorirData() {
+
+    let day = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+
+    let putColor = calendar.getElementsByTagName('td')[parseInt(day)+5];
+    putColor.style.backgroundColor = color;
+
+}
